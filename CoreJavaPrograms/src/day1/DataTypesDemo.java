37730a4927973ae5e0d6fe66da13f272807de1f4
thiru <thiru@Thirukumaran>
2025-09-26 21:48:44 +0530

    day2

--- CoreJavaPrograms/src/day1/DataTypesDemo.java
+++ CoreJavaPrograms/src/day1/DataTypesDemo.java
@@ -18,7 +18,7 @@ public class DataTypesDemo {
 		int marker = 512; 
 		double percentage = marker * 0.46f;
 		System.out.println("percentage :" + percentage);
-
+       
 	}
 
 }
--- CoreJavaPrograms/src/day1/FirstProgram.java
+++ CoreJavaPrograms/src/day1/FirstProgram.java
@@ -1,10 +1,15 @@
 package day1;
 
+import java.util.Scanner;
+
+
+
 public class FirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello");
+		System.out.println("Hello");
+		
 
 	}
 
